0648a8dc05d22f550f0823679fb0ccc0fcc5fa12
johnwilliamsmithjr <36016389+johnwilliamsmithjr@users.noreply.github.com>
2023-01-17 14:09:44 -0700

    updated during revisions

--- setup/MCMC/monthly_timestep_monthly_obs_MCMC.R
+++ setup/MCMC/monthly_timestep_monthly_obs_MCMC.R
@@ -1,6 +1,6 @@
 ## file: monthly_timestep_monthly_obs_MCMC.R
 ## sets up MCMC to be run for the monthly latent state model
-## with monthly observation resolution
+## with yearly observation resolution
 
 ## source MCMC functions
 source('./setup/MCMC/MCMC_funs.R')
@@ -15,6 +15,8 @@ source('./setup/MCMC/cpp_ss_likelihood.R')
 source('./setup/MCMC/get_latent_sample.R')
 source('./setup/MCMC/latent_initialization.R')
 
+nmonths <- 72
+
 ## likelihood
 ss_monthly_ll <- function(C_obs, C, init_mean, init_var, prec_obs, pars, fluxes, G, ndays, drivers, init_ev, r = 1, ...){
   native_step = cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))
@@ -23,7 +25,7 @@ ss_monthly_ll <- function(C_obs, C, init_mean, init_var, prec_obs, pars, fluxes,
   ## Initial Condition Likelihood 
   LL <- sum(dnorm(x = C_obs[1,], mean = init_mean, sd = sqrt(init_var), log = TRUE))
   ## Build Cpred
-  LAI_ = Cf_approx / LMA_e
+  LAI_ = LAI
   preds <- getDALECpredictions2(p_e = p, C = C, LAI = LAI_, maxt = drivers$maxt[1:nday],
                                 mint = drivers$mint[1:nday], G_obs = fluxes$G, ind = native_step-1, init_mean = init_mean, native_step = native_step,
                                 ...)
@@ -83,6 +85,16 @@ if (!('r' %in% ls())){
   cat('No r value supplied for datacloning, using default (r=1)\n')
 }
 
+C <- cbind(Cf_e, Cw_e, Cr_e, Clit_e, Csom_e)
+
+C <- C[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6)), ]
+
+C_obs <- matrix(NA, ncol = 5, nrow = 72)
+C_obs[c(seq(from = 1, to = 71, by = 1), 72), 1] <- Cf.obs[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))[c(seq(from = 1, to = 71, by = 1), 72)]]
+C_obs[c(seq(from = 1, to = 71, by = 1), 72), 2] <- Cw.obs[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))[c(seq(from = 1, to = 71, by = 1), 72)]]
+C_obs[c(seq(from = 1, to = 71, by = 1), 72), 3] <- Cr.obs[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))[c(seq(from = 1, to = 71, by = 1), 72)]]
+C_obs[c(seq(from = 1, to = 71, by = 1), 72), 4] <- Clit.obs[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))[c(seq(from = 1, to = 71, by = 1), 72)]]
+C_obs[c(seq(from = 1, to = 71, by = 1), 72), 5] <- Csom.obs[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))[c(seq(from = 1, to = 71, by = 1), 72)]]
 ## define getDALECpredictions2, a function to extract predictions on the fast and slow timescale to be fed into the MCMC routine and likelihood
 getDALECpredictions2 <- function(p_e, C, LAI, maxt, mint, Ca, lat_e, yearday, Nit_e, rad, N, G_obs, ind, init_mean, native_step, preds = NULL){
   ## get predictions from Cpred_m.cpp
@@ -95,7 +107,6 @@ getDALECpredictions2 <- function(p_e, C, LAI, maxt, mint, Ca, lat_e, yearday, Ni
   ## store tlapm coefficients for native timestep
   A <- cbind(preds$A_cf, preds$A_cw, preds$A_cr, preds$A_clit, preds$A_csom)
   b <- cbind(preds$b_cf, preds$b_cw, preds$b_cr, preds$b_clit, preds$b_csom)
-  #ind = ind + 1
   ## initialize tpalm coefficients for new timestep
   newA <- matrix(NA, ncol = 5, nrow = length(native_step))
   newB <- matrix(NA, ncol = 5, nrow = length(native_step))
@@ -124,19 +135,17 @@ getDALECpredictions2 <- function(p_e, C, LAI, maxt, mint, Ca, lat_e, yearday, Ni
 }
 
 ## create carbon stock observation dataframe
-C_obs <- cbind(Cf.obs, Cw.obs, Cr.obs, Clit.obs, Csom.obs)
-C_obs <- C_obs[1:nmonths,]
 
 ## create flux list
 fluxes_obs <- list(Lr_obs = Lr_obs, D_e_obs = D_e_obs, Rh1_obs = Rh1_obs, Rh2_obs = Rh2_obs, 
                    Ar_obs = Ar_obs, Af_obs = Af_obs, Lf_obs = Lf_obs, Ra_obs = Ra_obs, Lw_obs = Lw_obs, G = G_obs)
 
 ## store true latent states
-Cf <- Cf_e
-Cw <- Cw_e
-Cr <- Cr_e
-Clit <- Clit_e
-Csom <- Csom_e
+Cf <- Cf_e[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))]
+Cw <- Cw_e[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))]
+Cr <- Cr_e[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))]
+Clit <- Clit_e[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))]
+Csom <- Csom_e[cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))]
 
 # Set driver variables
 mint <- drivers_ev$mint[1:nday] 
@@ -165,15 +174,17 @@ options(warn = 2)
 # Compute LAI estimate to be fed into p_11 optimization routine
 LAI <- pmax(.1, Cf.obs/LMA_e)
 # Column bind matrices and build initial Cpred matrix
-C_obs <- cbind(Cf.obs, Cw.obs, Cr.obs, Clit.obs, Csom.obs)
 
 ## reset observation index
-missing_data_ind = seq(from = 1, to = nmonths, by = 1)
-#missing_data_ind = c(missing_data_ind, nmonths)
-ndays <- c(31, diff(cumsum(ndays)[missing_data_ind]))
-C_obs[-missing_data_ind,] <- NA
-C <- cbind(Cf, Cw, Cr, Clit, Csom)
 
+if (!('observation_index' %in% ls())){
+  missing_data_ind = 1:72
+  cat('No changes detected in observation index, using yearly observation data\n')
+} else{
+  missing_data_ind <- observation_index
+}
+
+ndays <- rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6)
 # Initalize matrix for p draws to be stored
 pars <- matrix(NA, nrow = chain_length, ncol = 16)
 # Set vector of upper and lower bounds for accept reject sampling
@@ -225,6 +236,7 @@ pars <- matrix(NA, nrow = 1, ncol = 16)
 
 if (!('par_inits' %in% ls())){
   pars[1,] <- c(.5*(pupper[1:11] + plower[1:11]), rep(1,5))
+  #pars[1,] <- c(.0017, .3,.18, .3, .002, .00015, .0015, .11, 1.2e-05, .172, 3)
   cat('No changes detected to parameter initializations, using default\n')
 } else{
   if (length(par_inits) != 16){
@@ -256,8 +268,15 @@ if (!('sd' %in% ls())){
 ## set column names
 colnames(pars) <- c(paste0('p', 1:11), paste0('precadd', 1:5))
 
+## use obs data to initialize latent states
 
-preds <- getDALECpredictions2(p_e = pars[1:11], C = C_samples[,,1], LAI = Cf_approx / LMA_e, maxt = maxt, mint = mint,
+C_samples[,1,1] <- C_obs[,1]
+C_samples[,2,1] <- C_obs[,2]
+C_samples[,3,1] <- C_obs[,3]
+C_samples[,4,1] <- C_obs[,4]
+C_samples[,5,1] <- C_obs[,5]
+
+preds <- getDALECpredictions2(p_e = pars[1:11], C = C_samples[,,1], LAI = LAI, maxt = maxt, mint = mint,
                               Ca = Ca, lat_e = lat_e, yearday = yearday, Nit_e = Nit_e, rad = rad, N = nday, G_obs = G_e, ind = cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))-1, 
                               init_mean = C.0[1,], native_step = cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6)))
 
@@ -284,12 +303,6 @@ if (!('block' %in% ls())){
   cat('No changes detected in block updates, using default blocks\n')
 }
 
-preds <- getDALECpredictions2(p_e = p_e, C = C, LAI = Cf_approx / LMA_e, maxt = maxt, mint = mint,
-                              Ca = Ca, lat_e = lat_e, yearday = yearday, Nit_e = Nit_e, rad = rad, N = 2190, G_obs = G_e, 
-                              ind = cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6))-1, 
-                              init_mean = init_mean, native_step = cumsum(rep(c(31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31), 6)))
-
-test <- cbind(Cf_approx, Cr_approx, Cw_approx, Clit_approx, Csom_approx)
 Cpredi <- preds$Cslow
 
 ## specify transformation
